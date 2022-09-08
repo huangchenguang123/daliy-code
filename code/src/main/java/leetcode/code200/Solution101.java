b8af41749d79c6cd374983f7d5c2613ac9d4c412
chuxing <923622908@qq.com>
2022-09-08 11:14:11 +0800

    [commit]: push 101

---
+++ code/src/main/java/leetcode/code200/Solution101.java
@@ -0,0 +1,49 @@
+package leetcode.code200;
+
+/**
+ * @date 2022/9/8 11:05
+ * @author huangchenguang
+ * @desc
+ */
+public class Solution101 {
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return dfs(root.left, root.right);
+    }
+
+    private boolean dfs(TreeNode left, TreeNode right) {
+        if (left == null && right != null) {
+            return false;
+        }
+        if (left != null && right == null) {
+            return false;
+        }
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return dfs(left.left, right.right) && dfs(left.right, right.left);
+    }
+
+}
